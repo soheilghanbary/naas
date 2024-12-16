02f23e93fb777ba7a0ace7f82fbaaf1dc47d9302
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-12-16 13:49:36 +0330

    update textfile area

--- src/components/common/text-field-area.tsx
+++ src/components/common/text-field-area.tsx
@@ -9,6 +9,8 @@ import {
 
 type TextFieldAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
   label: string
+  description?: string
+  error?: string
 }
 
 export const TextFieldArea = forwardRef(function MyInput(
@@ -19,6 +21,12 @@ export const TextFieldArea = forwardRef(function MyInput(
     <div className={cn('grid gap-2 [&>label]:text-sm', className)}>
       <Label>{label}</Label>
       <Textarea ref={ref} {...rest} />
+      {description && (
+        <span className="text-muted-foreground text-xs">{description}</span>
+      )}
+      {error && (
+        <span className="font-medium text-destructive text-xs">{error}</span>
+      )}
     </div>
   )
 })
