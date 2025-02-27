a6c61c6c340481c9d89aa60288d11c5c25a37069
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-02-28 01:41:59 +0330

    update more

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,7 +1,9 @@
-'use client'
-import { cn } from '@/lib/utils'
-import * as LabelPrimitive from '@radix-ui/react-label'
-import type * as React from 'react'
+"use client"
+
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
+
+import { cn } from "@/lib/utils"
 
 function Label({
   className,
@@ -11,7 +13,7 @@ function Label({
     <LabelPrimitive.Root
       data-slot="label"
       className={cn(
-        'select-none font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50',
+        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         className
       )}
       {...props}
