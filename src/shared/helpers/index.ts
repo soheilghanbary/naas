9fda3f97764910561f66844e83fb5000ad825270
Soheil Ghanbary <soli.ghanbary@gmail.com>
2026-02-22 21:26:24 +0330

    fix auth client imports

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -1,3 +1,6 @@
+import { headers } from 'next/headers'
+import { auth } from '@/server/lib/auth'
+
 export const getSession = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
